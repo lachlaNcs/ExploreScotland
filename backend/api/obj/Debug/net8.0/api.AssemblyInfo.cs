398c2378a6b75d74515c8f6b6e7e80591b0a0926
mark <mcmillanmark1@hotmail.com>
2024-11-12 09:55:32 +0000

    Added .gitignore file

--- backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ backend/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a737b14571bcdbe7f1660e356d2ee735d1311e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18de9b223579029a709bcc80bb94b68ce769f2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
